717664db2280320e18a9ae3602c206babd201838
Aaron Lehmann <alehmann@netflix.com>
2021-03-26 17:12:06 -0700

    Add retry on 5xx; add some logging
    This is currently triggering context timeouts.

--- util/resolver/retryhandler/retry.go
+++ util/resolver/retryhandler/retry.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/containerd/containerd/images"
+	remoteserrors "github.com/containerd/containerd/remotes/errors"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 )
@@ -18,13 +19,16 @@ func New(f images.HandlerFunc, logger func([]byte)) images.HandlerFunc {
 	return func(ctx context.Context, desc ocispec.Descriptor) ([]ocispec.Descriptor, error) {
 		backoff := time.Second
 		for {
+			logger([]byte("calling f"))
 			descs, err := f(ctx, desc)
 			if err != nil {
 				select {
 				case <-ctx.Done():
+					logger([]byte("context was cancelled"))
 					return nil, err
 				default:
 					if !retryError(err) {
+						logger([]byte("retryError returned false"))
 						return nil, err
 					}
 				}
@@ -48,6 +52,14 @@ func New(f images.HandlerFunc, logger func([]byte)) images.HandlerFunc {
 }
 
 func retryError(err error) bool {
+	// Retry on 5xx errors
+	var errUnexpectedStatus remoteserrors.ErrUnexpectedStatus
+	if errors.As(err, &errUnexpectedStatus) &&
+		errUnexpectedStatus.StatusCode >= 500 &&
+		errUnexpectedStatus.StatusCode <= 599 {
+		return true
+	}
+
 	if errors.Is(err, io.EOF) || errors.Is(err, syscall.ECONNRESET) || errors.Is(err, syscall.EPIPE) {
 		return true
 	}
